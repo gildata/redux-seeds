ffc6151e2d22bee70823bfbf78b5fad28d69af23
anonymous <xgqfrms@gmail.com>
2017-08-01 15:48:25 +0800

    Add files via upload

---
+++ redux-source-codes/compose.js
@@ -0,0 +1,90 @@
+/**
+ * Composes single-argument functions from right to left. The rightmost
+ * function can take multiple arguments as it provides the signature for
+ * the resulting composite function.
+ *
+ * @param {...Function} funcs The functions to compose.
+ * @returns {Function} A function obtained by composing the argument functions
+ * from right to left. For example, compose(f, g, h) is identical to doing
+ * (...args) => f(g(h(...args))).
+ */
+
+export default function compose(...funcs) {
+    if (funcs.length === 0) {
+        return arg => arg;
+    }
+    if (funcs.length === 1) {
+        return funcs[0];
+    }
+    return funcs.reduce(
+        (a, b) => (...args) => a(b(...args))
+    );
+}
+
+/* 
+
+// function reduce js
+
+// Array.prototype.reduce() & ...spread /...rest
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+
+
+
+https://www.sitepoint.com/map-reduce-functional-javascript/
+
+
+function compose(...funcs) {
+    if (funcs.length === 0) {
+        return arg => arg;
+    }
+    if (funcs.length === 1) {
+        return funcs[0];
+    }
+    return funcs.reduce(
+        (a, b) => (...args) => a(b(...args))
+    );
+}
+
+function func1(){
+    console.log(`a1`);
+}
+function func2(){
+    console.log(`b2`);
+}
+function func3(){
+    console.log(`c3`);
+}
+function func4(){
+    console.log(`d4`);
+}
+function func5(){
+    console.log(`e5`);
+}
+
+// object
+const funcs = {
+    func1,
+    func2,
+    func3,
+    func4,
+    func5
+};
+compose(...funcs);
+// undefined is not a function
+
+
+// array
+const funcs = [
+    func1,
+    func2,
+    func3,
+    func4,
+    func5
+];
+
+compose(...funcs);
+// (...args) => a(b(...args))
+
+
+*/
